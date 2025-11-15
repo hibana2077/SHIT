4b97483aa406d03d1bd67958252fe9c80cab5e52
hibana2077 <hibana2077@gmail.com>
2025-11-15 10:48:00 +1000

    feat: Add training scripts TNACT001.sh, TNBT001.sh, TNCT001.sh, TNDT001.sh, and TNECT001.sh for Cotton80 dataset

---
+++ script/tinynet/CT80/TNDCT001.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -P kf09
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=24GB
+#PBS -l walltime=28:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+module load ffmpeg/4.1.3
+source /scratch/rp06/sl5952/SHIT/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/SHIT/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../../..
+python3 -m src.train \
+    --model-name tinynet_d.in1k \
+    --dataset-name cotton80 \
+    --batch-size 32 \
+    --num-epochs 200 \
+    --lr 1e-4 \
+    --output-dir ./outputs/TNDT001 --num-classes 80 --val-split test >> TNDT001.log
